a7451e14ad8be074ca97ad7c2e2538d86a47ef8e
alan-giles-zesty <54852827+alan-giles-zesty@users.noreply.github.com>
2023-04-18 22:48:29 +0100

    adding cypress files

---
+++ cypress/e2e/weather-api/weather1.cy.js
@@ -0,0 +1,16 @@
+describe("Weather API", () => {
+
+  const weatherBaseUrl = "https://api.weather.gov";
+
+  context("GET alert type content", () => {
+    it("gets a list of alert types", () => {
+      cy.request("GET", `${weatherBaseUrl}/alerts/types`).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response).to.have.property('headers')
+        expect(response).to.have.property('duration')
+        cy.log("response body: " + response.body.eventTypes);
+        expect(response.body.eventTypes[0]).to.contains("911");
+      })
+    })
+  })
+})
