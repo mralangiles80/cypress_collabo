5be5a44ad10082b2e8deb7007d66cf94234aaf36
alan-giles-zesty <54852827+alan-giles-zesty@users.noreply.github.com>
2023-06-22 15:29:56 +0100

    js update

--- cypress/e2e/weather-api/weather1.cy.js
+++ cypress/e2e/weather-api/weather1.cy.js
@@ -4,12 +4,13 @@ describe("Weather API", () => {
 
   context("GET alert type content", () => {
     it("gets a list of alert types", () => {
+      let expectedEventType = '911'
       cy.request("GET", `${weatherBaseUrl}/alerts/types`).then((response) => {
         expect(response.status).to.eq(200);
         expect(response).to.have.property('headers')
         expect(response).to.have.property('duration')
         cy.log("response body: " + response.body.eventTypes);
-        expect(response.body.eventTypes[0]).to.contains("911");
+        expect(response.body.eventTypes[0]).to.contains(expectedEventType);
       })
     })
   })
