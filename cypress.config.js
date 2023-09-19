e6705e6507cea00c2d8851b70a45a5708b7efc16
ashleygraf101 <ashley.graf101@gmail.com>
2023-09-20 00:25:49 +1000

    html reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     supportFile: false,
     video: false,
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   }
 })
