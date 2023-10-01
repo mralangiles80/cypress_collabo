dc5f997eae997959de67baaf5721f62472ffdc5f
ashleygraf101 <ashley.graf101@gmail.com>
2023-10-01 19:29:24 +1100

    border tests

---
+++ cypress/e2e/weather-api/borders.cy.js
@@ -0,0 +1,37 @@
+describe("Weather API Tests", () => {
+
+  var latitudes = [];
+  var longtitudes = [];
+
+  for (var i = 70; i <= 90; i++) { longtitudes.push(i);  }
+  for (var i = 30; i <= 50; i++) { latitudes.push(i);  }
+  const regionalOffice = 'TOP';
+  const randomLatitude = latitudes[Math.floor(Math.random() * latitudes.length)];
+  const randomLongtitude = longtitudes[Math.floor(Math.random() * longtitudes.length)];
+
+  context("GET borders", () => {
+    it("checks the status of different regions in relation to US borders", () => {
+      cy.request({ 
+        url: `gridpoints/${regionalOffice}/25,133/forecast`,
+        failOnStatusCode:false,
+      }).then((response) => {
+            expect(response.status).to.eq(500)
+      }),  // australia
+      cy.request({
+        url: `gridpoints/${regionalOffice}/32,116/forecast`,
+        failOnStatusCode:false,
+      }).then((response) => {
+            expect(response.status).to.eq(500)
+      }), // mexican border
+      cy.request("GET", `gridpoints/${regionalOffice}/${randomLatitude},${randomLongtitude}/forecast`).then((response) => {
+        expect(response.status).to.eq(200); // mid-west somewhere
+      }),
+      cy.request("GET", `gridpoints/${regionalOffice}/37,94/forecast`).then((response) => {
+        expect(response.status).to.eq(200); // near the mexican border
+      }),
+      cy.request("GET", `gridpoints/${regionalOffice}/18,66/forecast`).then((response) => {
+        expect(response.status).to.eq(200); // puerto rico
+      })
+    })
+  })     
+})
--- cypress/e2e/weather-api/temperature.cy.js
+++ cypress/e2e/weather-api/temperature.cy.js
@@ -12,16 +12,16 @@ describe("Weather API Tests", () => {
   context("GET temperature", () => {
     it("tests it meets temperature rules", () => {
       cy.request("GET", `gridpoints/${regionalOffice}/${randomLatitude},${randomLongtitude}/forecast`).then((response) => {
-      cy.dumpLog('response body', response.body)
-      expect(response.status).to.eq(200);
-      var forecastPeriods = response.body.properties.periods;
-      forecastPeriods.forEach(function(forecastPeriod) {
-        expect(forecastPeriod.temperature % 1).to.equal(0) // integer
-        cy.get(forecastPeriod.temperature).should('not.be.empty');
-        expect(forecastPeriod.temperature).to.be.greaterThan(-100); 
-        expect(forecastPeriod.temperature).to.be.lessThan(200);
-      });
+        cy.dumpLog('response body', response.body)
+        expect(response.status).to.eq(200);
+        var forecastPeriods = response.body.properties.periods;
+        forecastPeriods.forEach(function(forecastPeriod) {
+          expect(forecastPeriod.temperature % 1).to.equal(0) // integer
+          cy.get(forecastPeriod.temperature).should('not.be.empty');
+          expect(forecastPeriod.temperature).to.be.greaterThan(-100); 
+          expect(forecastPeriod.temperature).to.be.lessThan(200);
+        });
+      })
     })
-  })
- })     
+  })     
 })
