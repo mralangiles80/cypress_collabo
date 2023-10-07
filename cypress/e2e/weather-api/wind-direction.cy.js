9679cb8b6ceae4ccd56698107e3d0d69094c97f0
ashleygraf101 <ashley.graf101@gmail.com>
2023-10-08 00:30:21 +1100

    add wind direction tests

--- cypress/e2e/weather-api/wind-direction.cy.js
+++ cypress/e2e/weather-api/wind-direction.cy.js
@@ -1,4 +1,4 @@
-describe("Temperature API Tests", () => {
+describe("Wind Direction API Tests", () => {
 
   var latitudes = [];
   var longtitudes = [];
@@ -9,16 +9,17 @@ describe("Temperature API Tests", () => {
   const randomLatitude = latitudes[Math.floor(Math.random() * latitudes.length)];
   const randomLongtitude = longtitudes[Math.floor(Math.random() * longtitudes.length)];
 
-  context("GET temperature", () => {
-    it("tests it meets temperature rules", () => {
+  context("GET wind direction", () => {
+    it("meets expectations for wind directions", () => {
       cy.request("GET", `gridpoints/${regionalOffice}/${randomLatitude},${randomLongtitude}/forecast`).then((response) => {
         expect(response.status).to.eq(200);
         var forecastPeriods = response.body.properties.periods;
         forecastPeriods.forEach(function(forecastPeriod) {
-          expect(forecastPeriod.temperature % 1).to.equal(0) // integer
-          cy.get(forecastPeriod.temperature).should('not.be.empty');
-          expect(forecastPeriod.temperature).to.be.greaterThan(-100); 
-          expect(forecastPeriod.temperature).to.be.lessThan(200);
+          expect(forecastPeriod.windDirection).to.be.a('string');
+          expect(forecastPeriod.windDirection).to.not.be.empty;
+          expect(forecastPeriod.windDirection).to.have.length.of.at.most(3);
+          expect(forecastPeriod.windDirection).to.match(/[A-Z]+/);
+          expect(forecastPeriod.windDirection).to.be.oneOf(['N', 'S', 'E', 'W', 'SW', 'SE', 'NE', 'NW', 'NNE', 'ENE', 'ESE', 'SSE', 'SSW', 'WSW', 'WNW', 'NNW']);
         });
       })
     })
