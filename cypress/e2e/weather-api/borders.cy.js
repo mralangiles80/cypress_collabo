2308e0afe75980e89727b815d70ab68941d8bf4d
alan-giles-zesty <54852827+alan-giles-zesty@users.noreply.github.com>
2023-11-14 15:23:05 +0000

    adding first fixture file for test data

--- cypress/e2e/weather-api/borders.cy.js
+++ cypress/e2e/weather-api/borders.cy.js
@@ -1,35 +1,37 @@
+const regionalOffices = require('../../fixtures/regional-offices.json')
+var latitudes = [];
+var longtitudes = [];
+
 describe("Border API Tests", () => {
 
-  var latitudes = [];
-  var longtitudes = [];
+  const regionalOffice = regionalOffices[0]
 
-  for (var i = 70; i <= 90; i++) { longtitudes.push(i);  }
-  for (var i = 30; i <= 50; i++) { latitudes.push(i);  }
-  const regionalOffice = 'TOP';
+  for (var i = regionalOffice.minLongtitude; i <= regionalOffice.maxLongtitude; i++) { longtitudes.push(i);  }
+  for (var i = regionalOffice.minLatitude; i <= regionalOffice.maxLatitude; i++) { latitudes.push(i);  }
   const randomLatitude = latitudes[Math.floor(Math.random() * latitudes.length)];
   const randomLongtitude = longtitudes[Math.floor(Math.random() * longtitudes.length)];
 
   context("GET borders", () => {
     it("checks the status of different regions in relation to US borders", () => {
       cy.request({ 
-        url: `gridpoints/${regionalOffice}/25,133/forecast`,
+        url: `gridpoints/${regionalOffice.code}/25,133/forecast`,
         failOnStatusCode:false,
       }).then((response) => {
             expect(response.status).to.eq(500)
       }),  // australia
       cy.request({
-        url: `gridpoints/${regionalOffice}/32,116/forecast`,
+        url: `gridpoints/${regionalOffice.code}/32,116/forecast`,
         failOnStatusCode:false,
       }).then((response) => {
             expect(response.status).to.eq(500)
       }), // mexican border
-      cy.request("GET", `gridpoints/${regionalOffice}/${randomLatitude},${randomLongtitude}/forecast`).then((response) => {
+      cy.request("GET", `gridpoints/${regionalOffice.code}/${randomLatitude},${randomLongtitude}/forecast`).then((response) => {
         expect(response.status).to.eq(200); // mid-west somewhere
       }),
-      cy.request("GET", `gridpoints/${regionalOffice}/37,94/forecast`).then((response) => {
+      cy.request("GET", `gridpoints/${regionalOffice.code}/37,94/forecast`).then((response) => {
         expect(response.status).to.eq(200); // near the mexican border
       }),
-      cy.request("GET", `gridpoints/${regionalOffice}/18,66/forecast`).then((response) => {
+      cy.request("GET", `gridpoints/${regionalOffice.code}/18,66/forecast`).then((response) => {
         expect(response.status).to.eq(200); // puerto rico
       })
     })
--- cypress/e2e/weather-api/temperature.cy.js
+++ cypress/e2e/weather-api/temperature.cy.js
@@ -1,17 +1,19 @@
+const regionalOffices = require('../../fixtures/regional-offices.json')
+var latitudes = [];
+var longtitudes = [];
+
 describe("Temperature API Tests", () => {
 
-  var latitudes = [];
-  var longtitudes = [];
+  const regionalOffice = regionalOffices[0]
 
-  for (var i = 70; i <= 90; i++) { longtitudes.push(i);  }
-  for (var i = 30; i <= 50; i++) { latitudes.push(i);  }
-  const regionalOffice = 'TOP';
+  for (var i = regionalOffice.minLongtitude; i <= regionalOffice.maxLongtitude; i++) { longtitudes.push(i);  }
+  for (var i = regionalOffice.minLatitude; i <= regionalOffice.maxLatitude; i++) { latitudes.push(i);  }
   const randomLatitude = latitudes[Math.floor(Math.random() * latitudes.length)];
   const randomLongtitude = longtitudes[Math.floor(Math.random() * longtitudes.length)];
 
   context("GET temperature", () => {
     it("tests it meets temperature rules", () => {
-      cy.request("GET", `gridpoints/${regionalOffice}/${randomLatitude},${randomLongtitude}/forecast`).then((response) => {
+      cy.request("GET", `gridpoints/${regionalOffice.code}/${randomLatitude},${randomLongtitude}/forecast`).then((response) => {
         expect(response.status).to.eq(200);
         var forecastPeriods = response.body.properties.periods;
         forecastPeriods.forEach(function(forecastPeriod) {
